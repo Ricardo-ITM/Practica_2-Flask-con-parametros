c836eb85dad5f3cd87c048f4a524a6b0e689c5c3
Ricardo-ITM <programa.dream@gmail.com>
2025-03-20 18:21:09 -0700

    Enhance welcome route to support multiple parameters and responses

--- practica1.py
+++ practica1.py
@@ -1,5 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def welcome():
-    return 'Bienvenido'
+@app.route('/wellcome/')
+
+@app.route('/wellcome/<name>')
+
+@app.route('/wellcome/<int:ncontrol>')
+
+@app.route('/wellcome/<name>/<int:ncontrol>')
+
+def bienvenido(name=None,ncontrol=None):
+
+    if name== None and ncontrol==None:
+
+        return 'Bienvenido '
+
+    if name!= None and ncontrol == None:
+
+        return f'Bienvenido {name}'
+
+    if name == None and ncontrol != None:
+
+        return f'El número recibido es: {ncontrol}'
+
+    else:
+
+        return f'Bienvenido {name} tu numero de control es: {ncontrol}'
+
